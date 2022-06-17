b47ae4b45c6276aa02ea43c2d9088cc32eb28224
zernonia <zernonia@gmail.com>
2022-06-17 22:59:18 +0800

    write main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,43 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import core from '@actions/core'
+import {createClient} from '@supabase/supabase-js'
+import {promises as fs} from 'fs'
+
+const PATH = process.env.GITHUB_WORKSPACE
+  ? `${process.env.GITHUB_WORKSPACE}/screenshots/`
+  : `screenshots/`
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const bucket = core.getInput('bucket')
+    const supabaseUrl = process.env.SUPABASE_URL
+    const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+    if (!supabaseUrl || !supabaseAnonKey) {
+      core.setOutput('result', 'No videos or screenshots found!')
+      return
+    }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
-    core.setOutput('time', new Date().toTimeString())
+    const files = await fs.readdir(PATH)
+    if (!files.length) {
+      core.setOutput('result', 'No videos or screenshots found!')
+      return
+    }
+
+    // eslint-disable-next-line @typescript-eslint/prefer-for-of
+    for (let i = 0; i < files.length; i++) {
+      const filename = files[i]
+      const buffer = await fs.readFile(filename)
+
+      const {data, error} = await supabase.storage
+        .from(bucket)
+        .upload(filename, buffer, {
+          contentType: 'image/jpeg'
+        })
+
+      if (error) throw new Error(error.message)
+      core.setOutput('result', data?.Key)
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
